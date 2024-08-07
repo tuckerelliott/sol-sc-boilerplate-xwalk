ef0afe2749b141b31faf6f40de993386a0660f19
Tucker Elliott <telliott@adobe.com>
2024-08-07 16:26:20 -0400

    CF by slug

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -1,44 +1,53 @@
-/* eslint-disable no-underscore-dangle */
-export default async function decorate(block) {
-  const aempublishurl = 'https://publish-p99452-e916802.adobeaemcloud.com';
-  const aemauthorurl = 'https://author-p99452-e916802.adobeaemcloud.com';
-  const persistedquery = '/graphql/execute.json/aem-demo-assets/native-app-adventure';
-  // const offerpath = '/content/dam/aem-demo-assets/en/adventures/bali-surf-camp/bali-surf-camp';
-  const variationname = 'main';
-  const url = block.querySelector(':scope div:nth-child(1) > div').innerHTML.trim();
-  //const offerpath = block.querySelector(':scope div:nth-child(1) > div').innerHTML.trim();
-  // const offerpath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
-  // const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML.trim();
 
-  /*
-  const url = window.location && window.location.origin && window.location.origin.includes('author')
-    ? `${aemauthorurl}${persistedquery};apath=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`
-    : `${aempublishurl}${persistedquery};apath=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`;
-  const options = { credentials: 'include' };
-*/
-  console.log(url);
+// put your AEM publish address here
+const AEM_HOST = "https://publish-p99452-e916802.adobeaemcloud.com";
 
-  const cfReq = await fetch(url)
-    .then((response) => response.json())
-    .then((contentfragment) => {
-      let offer = '';
-      if (contentfragment.data) {
-        offer = contentfragment.data.adventureByPath.item;
-      }
-      return offer;
-    });
+export default function decorate(block) {
 
-  // const itemId = `urn:aemconnection:${offerpath}/jcr:content/data/master`;
-  const itemId = `${url}/jcr:content/data/master`;
+  const slugDiv = block.querySelector('div:nth-child(1)'); 
+  const slugID = document.createElement('div');
+  slugID.id = 'slug';
+  slugDiv.replaceWith(slugID);
+  slugID.innerHTML = `${slugDiv.innerHTML}`;
+  const slugTemp = slugID.innerHTML.replace(/<div data-aue-prop=\"text\" data-aue-label=\"Slug\" data-aue-type=\"text\">|<div>|<\/div>/g, '');
+  const slug = slugTemp.match(/\S+/g);
+  
+  const quoteDiv = block.querySelector('div:last-of-type');
+  const adventureDiv = document.createElement('div');
+  adventureDiv.id = "adventure-" + slug; 
+  quoteDiv.replaceWith(adventureDiv);
 
-  block.innerHTML = `
-  <div class='banner-content' data-aue-resource=${itemId} data-aue-type="reference" data-aue-filter="cf">
-      <div data-aue-prop="heroImage" data-aue-type="media" class='banner-detail' style="background-image: linear-gradient(90deg,rgba(0,0,0,0.6), rgba(0,0,0,0.1) 80%) ,url(${cfReq.primaryImage._publishUrl});">
-          <p data-aue-prop="headline" data-aue-type="text" class='pretitle'>${cfReq.title}</p>
-          <p data-aue-prop="detail" data-aue-type="richtext" class='detail'>${cfReq.description.plaintext}</p>
-      </div>
-      <div class='banner-logo'>
-      </div>
-  </div>
-`;
+fetch(AEM_HOST + '/graphql/execute.json/aem-demo-assets/adventure-by-slug;slug=' + slug)
+.then(response => response.json())
+.then(response => {
+
+const backgroundImage = response.data.adventureList.items[0].primaryImage._path;
+document.getElementById(adventureDiv.id).innerHTML = "<section><img src=" + AEM_HOST + backgroundImage + "></section>";  
+
+const adventureTitle = response.data.adventureList.items[0].title;
+document.getElementById(adventureDiv.id).innerHTML += "<section><h3>"+ adventureTitle + "</h3></section>";
+
+const adventureDesc = response.data.adventureList.items[0].description.plaintext;
+document.getElementById(adventureDiv.id).innerHTML += "<section>" + adventureDesc + "</section>";
+
+const adventureType = response.data.adventureList.items[0].adventureType;
+document.getElementById(adventureDiv.id).innerHTML += "<section>" + "Adventure Type: " + adventureType + "</section>";
+
+const tripLength = response.data.adventureList.items[0].tripLength;
+document.getElementById(adventureDiv.id).innerHTML += "<section>" +"Trip Length: " + tripLength + "</section>";
+
+const tripDifficulty = response.data.adventureList.items[0].difficulty;
+document.getElementById(adventureDiv.id).innerHTML += "<section>" + "Difficulty: " + tripDifficulty + "</section>";
+
+const groupSize = response.data.adventureList.items[0].groupSize;
+document.getElementById(adventureDiv.id).innerHTML += "<section>" + "Group Size: " + groupSize + "</section>";
+
+const tripItinerary= response.data.adventureList.items[0].itinerary.html;
+document.getElementById(adventureDiv.id).innerHTML += "<section>" + "Itinerary: </br>" + tripItinerary + "</section>";
+
+})
+.catch(error => {
+  console.log('Error fetching data:', error);
+});
+
 }
