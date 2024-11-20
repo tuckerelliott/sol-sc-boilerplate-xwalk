56a7ecadcc7c02ecf553113f879bc0192f1bdf11
Chris Brown <chrbrown@adobe.com>
2024-11-20 15:08:18 -0500

    "Updated template literals in content-fragment.js to use backticks instead of double quotes."

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -19,25 +19,25 @@ export default function decorate(block) {
     .then((response) => response.json())
     .then((response) => {
       const backgroundImage = response.data.adventureList.items[0].primaryImage._path;
-      document.getElementById(adventureDiv.id).innerHTML = '<section><img src=${AEM_HOST}${backgroundImage}></section>';
+      document.getElementById(adventureDiv.id).innerHTML = `<section><img src=${AEM_HOST}${backgroundImage}></section>`;
 
       const adventureTitle = response.data.adventureList.items[0].title;
-      document.getElementById(adventureDiv.id).innerHTML += '<section><h3>${adventureTitle}</h3></section>';
+      document.getElementById(adventureDiv.id).innerHTML += `<section><h3>${adventureTitle}</h3></section>`;
 
       const adventureDesc = response.data.adventureList.items[0].description.plaintext;
-      document.getElementById(adventureDiv.id).innerHTML += '<section>${adventureDesc}</section>';
+      document.getElementById(adventureDiv.id).innerHTML += `<section>${adventureDesc}</section>`;
 
       const { adventureType } = response.data.adventureList.items[0];
-      document.getElementById(adventureDiv.id).innerHTML += '<section>Adventure Type: ${adventureType}</section>';
+      document.getElementById(adventureDiv.id).innerHTML += '<section>' + `Adventure Type: ${adventureType}</section>`;
 
       const { tripLength } = response.data.adventureList.items[0];
-      document.getElementById(adventureDiv.id).innerHTML += '<section>Trip Length: ${tripLength}</section>';
+      document.getElementById(adventureDiv.id).innerHTML += '<section>' + `Trip Length: ${tripLength}</section>`;
 
       const tripDifficulty = response.data.adventureList.items[0].difficulty;
-      document.getElementById(adventureDiv.id).innerHTML += '<section>Difficulty: ${tripDifficulty}</section>';
+      document.getElementById(adventureDiv.id).innerHTML += '<section>' + `Difficulty: ${tripDifficulty}</section>`;
 
       const { groupSize } = response.data.adventureList.items[0];
-      document.getElementById(adventureDiv.id).innerHTML += '<section>Group Size: ${groupSize}</section>';
+      document.getElementById(adventureDiv.id).innerHTML += '<section>' + `Group Size: ${groupSize}</section>`;
       /*
 const tripItinerary= response.data.adventureList.items[0].itinerary.html;
 document.getElementById(adventureDiv.id).innerHTML += "<section>" + "Itinerary: </br>" + tripItinerary + "</section>";
